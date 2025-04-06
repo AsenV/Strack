c8d893315dde016249d6589370946957dc642d91
AsenV <asenxv@gmail.com>
2025-04-06 14:51:45 -0300

    First Commit

---
+++ app/src/main/java/com/example/strack/ui/theme/Color.kt
@@ -0,0 +1,22 @@
+package com.example.strack.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+
+
+// dark
+val Purple80 = Color(0xFF0021FF)
+val PurpleGrey80 = Color(0xFF293AA1)
+val Pink80 = Color(0xFF3054FF)
+
+// light
+val Purple40 = Color(0xFF304BFF)
+val PurpleGrey40 = Color(0xFF4553AD)
+val Pink40 = Color(0xFF6981FF)
+
+val Black = Color(0xFF000000)
+val LightBlack = Color(0xFF1F1F1F)
+val White = Color(0xFFFFFFFF)
+val DarkWhite = Color(0xFFF1F1F1)
+val LightGray = Color(0xFFD9D9D9)
+val DarkGray = Color(0xFF484848)
