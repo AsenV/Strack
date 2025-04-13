46175971e684a6185dae36d6c72e87640fd0b3bd
AsenV <asenxv@gmail.com>
2025-04-12 23:22:33 -0300

    Minor Fixes

---
+++ app/src/main/java/com/example/strack/ui/theme/Color.kt
@@ -0,0 +1,22 @@
+package com.example.strack.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+
+
+// dark
+val Purple80 = Color(0xFF0021FF)
+val PurpleGrey80 = Color(0xFF3D71FF)
+val Pink80 = Color(0xFFFFC430)
+
+// light
+val Purple40 = Color(0xFF0021FF)
+val PurpleGrey40 = Color(0xFF3D71FF)
+val Pink40 = Color(0xFFFFC430)
+
+val Black = Color(0xFF000000)
+val LightBlack = Color(0xFF1F1F1F)
+val White = Color(0xFFFFFFFF)
+val DarkWhite = Color(0xFFF1F1F1)
+val LightGray = Color(0xFFD9D9D9)
+val DarkGray = Color(0xFF484848)
